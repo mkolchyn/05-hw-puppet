452da11c54327cb69d932150d071a1ffb3fe9710
mkolchyn <112177997+mkolchyn@users.noreply.github.com>
2023-01-04 21:10:44 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -62,3 +62,16 @@ node slave2 {
     enable => false,
   }
 }
+
+
+
+node mineserver.puppet {
+  service { 'firewalld':
+    ensure => stopped,
+    enable => false,
+  }
+  package { 'httpd':
+    ensure => installed,
+    name   => httpd,
+  }  
+}
