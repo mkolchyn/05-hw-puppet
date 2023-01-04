6d1734f2742b130d12e05f871b291c813d272995
mkolchyn <112177997+mkolchyn@users.noreply.github.com>
2023-01-04 22:53:41 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -78,6 +78,7 @@ node mineserver.puppet {
   exec { 'download minecraft server':
     cwd     => '/opt/minecraft',
     command => 'wget https://piston-data.mojang.com/v1/objects/c9df48efed58511cdd0213c56b9013a7b5c9ac1f/server.jar',
+    path    => "/usr/bin",
     require => File['/opt/minecraft'],  
   }
   exec { 'init start server':
