2cbf3c657530ec2698b36183f390fb3a15cd15de
mkolchyn <112177997+mkolchyn@users.noreply.github.com>
2023-01-04 21:50:01 +0300

    Update init.pp

--- site/minecraft/manifest/init.pp
+++ site/minecraft/manifest/init.pp
@@ -1,18 +1,5 @@
 class minecraft {
-  package {  'java':
-    name => java-17-openjdk.x86_64
-    ensure => present,
-  }
-  
-#  file { '/etc/systemd/system/minecraft.service':
-#    ensure => file,
-#    source => 'puppet:///modules/minecraft/minecraft.service',
-#  }
-  
   file { '/opt/minecraft':
     ensure => directory,
   }
-  
-  
-  
 }
