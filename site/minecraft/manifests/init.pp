15011ce4327f0febe0f706fd202c307068e12763
mkolchyn <112177997+mkolchyn@users.noreply.github.com>
2023-01-05 13:57:33 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,5 +1,18 @@
 class minecraft {
-
+  package {  'java':
+    name   => java-17-openjdk,
+    ensure => present,
+  }
+  file { '/opt/minecraft':
+    ensure => directory,
+  }
+  wget::retrieve { "download minecraft server":
+    source      => 'https://piston-data.mojang.com/v1/objects/c9df48efed58511cdd0213c56b9013a7b5c9ac1f/server.jar',
+    destination => '/opt/minecraft/',
+    timeout     => 0,
+    verbose     => false,
+    require => File['/opt/minecraft'],  
+  }
 
   file { '/opt/minecraft/server.properties':
     ensure => file,
