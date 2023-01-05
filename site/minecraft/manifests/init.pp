c09731fef440d9d87b38e8cb00cccab182a28167
mkolchyn <112177997+mkolchyn@users.noreply.github.com>
2023-01-05 13:25:02 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,4 +2,8 @@ class minecraft {
   file { '/opt/asdasdasdasdasdasdasd':
     ensure => directory,
   }
+  file { '/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///site/minecraft/minecraft.service',
+  }
 }
