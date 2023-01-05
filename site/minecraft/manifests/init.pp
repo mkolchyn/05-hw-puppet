24fd0511d5705b9567fb65b2b1bacc4b9f423980
mkolchyn <112177997+mkolchyn@users.noreply.github.com>
2023-01-05 13:42:18 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,9 +1,18 @@
 class minecraft {
-  file { '/opt/asdasdasdasdasdasdasd':
-    ensure => directory,
+
+
+  file { '/opt/minecraft/server.properties':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/server.properties',
   }
   file { '/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
+    require => File['/opt/minecraft/server.properties'],    
+  }
+  service { 'firewalld':
+    ensure => running,
+    enable => true,
+    require => File['/etc/systemd/system/minecraft.service'],
   }
 }
